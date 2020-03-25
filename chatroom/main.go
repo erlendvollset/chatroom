e2e96dca52aa5bf1c10dcea083b415c11b351908
erlendcognite <erlend.vollset@cognite.com>
2020-03-25 20:30:43 +0100

    Take host and port as command line arguments

--- chatroom/main.go
+++ chatroom/main.go
@@ -7,26 +7,37 @@ import "github.com/erlendvollset/chatroom/server"
 import "os"
 import "strings"
 
-func main() {
+var userName *string
+var port *string
+var host *string
+
+func parseClientCommand() {
 	clientCommand := flag.NewFlagSet("client", flag.ExitOnError)
-	userName := clientCommand.String("N", "Anon", "The name of the user")
+	userName = clientCommand.String("n", "Anon", "The name of the user")
+	host = clientCommand.String("h", "localhost", "The host for the chatroom server")
+	port = clientCommand.String("p", "8080", "The port of the chatroom server")
+	clientCommand.Parse(os.Args[2:])
+}
 
+func parseServerCommand() {
 	serverCommand := flag.NewFlagSet("server", flag.ExitOnError)
+	host = serverCommand.String("h", "localhost", "The host for the chatroom server")
+	port = serverCommand.String("p", "8080", "The port of the chatroom server")
+	serverCommand.Parse(os.Args[2:])
+}
 
-	host := "localhost"
-	port := "8080"
-
+func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("Usage: chatroom {client|server} [args]")
 		os.Exit(0)
 	}
 	switch strings.ToLower(os.Args[1]) {
 	case "client":
-		clientCommand.Parse(os.Args[2:])
-		client.New(userName).Start(host, port)
+		parseClientCommand()
+		client.New(userName).Start(*host, *port)
 	case "server":
-		serverCommand.Parse(os.Args[2:])
-		server.New().Start(host, port)
+		parseServerCommand()
+		server.New().Start(*host, *port)
 	default:
 		fmt.Printf("%q is not valid command.\n", os.Args[1])
 		os.Exit(2)
